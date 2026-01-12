1950a199db76d107e55eba8522f3668efe90a364
gianzk <g.gutierrez@uni.pe>
2026-01-12 17:15:05 -0500

    add modulo1

---
+++ proyecto_v1/main.py
@@ -0,0 +1,27 @@
+#Realiza un menu itearctivo para el sistema inmobiliario 
+# crea un diccionario de usuarios 
+# Agregar usuarios
+# realiza un login
+# Cerrar session
+# Crear producto 
+# Listar Productos
+# Detalle de un producto
+# Editar producto
+# Cambiar de estado 
+# Registrar cliente
+# evaluacion de clientes
+# Listar Clientes
+# Estados de cliente
+# Crear transaccion de venta
+# generar simulacion : Monto , TEA , Plazo
+# cuota : Mxi/1-(1+i)-n
+# Asociar cliente a producto
+# Marcar producto como reservado o vendido
+# buscador de producto
+# filtro de producto
+# Pagos de cliente
+# documentos asociados a propiedad
+# Reporte de ventas
+# Reporte de ingresos 
+
+
